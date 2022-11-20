e36eb88e1a2ad4e1db2c6eb25ddb6d5843f279e4
LTxai <81376256+LTxai@users.noreply.github.com>
2022-11-20 16:48:07 -0300

    Atualizei o resultado do primeiro placar (QATxECU)

--- frontend/react-sortable-table/src/components/TableBody.js
+++ frontend/react-sortable-table/src/components/TableBody.js
@@ -4,12 +4,12 @@ const TableBody = ({ tableData, columns }) => {
   return (
     <Tbody>
       {tableData.map((data, index) => {
-        data.points = data.guess * 5 + data.goals1 * 2 + data.goals2 * 2;
+        data.points = data.guess * 5 + data.goals * 2;
         data.position = index + 1
         return (
           <Tr key={data.id}>
             {columns.map(({ accessor }, index) => {
-              const tData = data[accessor] ? data[accessor] : "";
+              const tData = data[accessor] ? data[accessor] : 0;
               return <Td textAlign={"center"} key={accessor}>{index !== 0 ? tData : data.position}</Td>;
             })}
           </Tr>
